eb0e61d9edb21195cb1c19e706aa112c6661dfa2
Domenic Curro <domenic.curro@gmail.com>
2014-09-08 09:58:03 -0400

    pivotal-domenic-curro - Removed unused usings.

--- push-wp81-sample/push-wp81-sample.WindowsPhone/MainPage.xaml.cs
+++ push-wp81-sample/push-wp81-sample.WindowsPhone/MainPage.xaml.cs
@@ -2,21 +2,13 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Linq;
 using System.Net;
-using System.Runtime.InteropServices.WindowsRuntime;
 using System.Text;
 using System.Threading.Tasks;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.Networking.PushNotifications;
 using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
