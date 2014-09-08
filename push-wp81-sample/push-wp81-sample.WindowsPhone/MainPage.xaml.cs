a1713984888b8b5c68595cfa75d5087701dda332
pivotal-domenic-curro <dcurro@pivotal.io>
2014-09-08 16:07:35 -0400

    @pivotal-oksana-zvarych/@pivotal-domenic-curro - Removed unused includes.

--- push-wp81-sample/push-wp81-sample.WindowsPhone/MainPage.xaml.cs
+++ push-wp81-sample/push-wp81-sample.WindowsPhone/MainPage.xaml.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Net;
-using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using Windows.Networking.PushNotifications;
